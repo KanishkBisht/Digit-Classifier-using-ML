9790d21626fa755ddcd78af78e42242642bdc707
KanishkBisht <kanishkbisht669@gmail.com>
2022-04-18 00:03:49 +0530

    first commit

---
+++ app/src/main/java/com/example/handwriting/TouchPadObj.java
@@ -0,0 +1,20 @@
+package com.example.digitrecognition;
+
+import android.graphics.Path;
+
+public class TouchPadObj {
+
+    public int color;
+    public boolean emboss;
+    public boolean blur;
+    public int strokeWidth;
+    public Path path;
+
+    public TouchPadObj(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
+    }
+}
